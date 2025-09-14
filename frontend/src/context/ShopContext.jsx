dc30f508c3187e28d4b72608890aa036416a8dfe
kuruva srinivasulu <balayya375@gmail.com>
2025-09-14 18:20:43 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider=(props)=>{
 
     const currency='₹'
     const delivery_fee=10
-    const backendUrl=import.meta.env.VITE_BACKEND_URL
+    const backendUrl="https://ecommerce-lscq.onrender.com"
     const [search,setSearch]=useState('')
     const [showSearch, setShowSearch]=useState(false)
     const [cartItems, setCartItems]=useState({})
